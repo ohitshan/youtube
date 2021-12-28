640279c93fc966bf1b02f07097daf1f83eecd9d8
han you <rkddkwl1364@gmail.com>
2021-12-29 02:54:31 +0900

    Updates

---
+++ precache-manifest.f463637619373708d678e2d7f7c63a18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b073084a26168a73ab14e4d19117fef",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "be68d75424eb60a86d56",
+    "url": "/youtube/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "a158d5914180f30d742a",
+    "url": "/youtube/static/css/main.fbe122e3.chunk.css"
+  },
+  {
+    "revision": "be68d75424eb60a86d56",
+    "url": "/youtube/static/js/2.e501b8ab.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/youtube/static/js/2.e501b8ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a158d5914180f30d742a",
+    "url": "/youtube/static/js/main.fe96f710.chunk.js"
+  },
+  {
+    "revision": "a1aaf11399ae9e2ccacc",
+    "url": "/youtube/static/js/runtime-main.1afe002e.js"
+  }
+]);
